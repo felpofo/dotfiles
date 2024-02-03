c5cfafabc0354b89a7a969594aa970997ea80a1c
felpofo <felpofo@proton.me>
2024-02-02 21:24:00 -0300

    bspwm quit script to close all background apps too

--- scripts/exit.fish
+++ scripts/exit.fish
@@ -1,3 +1,26 @@
 #!/bin/fish
 
+killall border.fish
+killall mpd.fish
+
+mpd --kill
+
+killall dunst
+killall sxhkd
+
+killall polybar
+
+killall easyeffects
+killall wireplumber
+killall pipewire
+killall pipewire-pulse
+
+killall polkit-mate-authentication-agent-1
+killall xdg-desktop-portal-gtk
+killall xdg-desktop-portal
+
+killall xdg-document-portal
+killall xdg-permission-store
+
+bspc quit
 
