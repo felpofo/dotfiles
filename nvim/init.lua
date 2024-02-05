b4b05a10f8c433cca016936092d5a93612a89163
felpofo <felpofo@proton.me>
2024-02-05 01:31:39 -0300

    nvim: just lazy

---
+++ nvim/init.lua
@@ -0,0 +1,5 @@
+require 'bootstrap'
+
+require 'lazy'.setup('plugins', {
+  defaults = { lazy = true }
+})
---
+++ nvim/lua/bootstrap.lua
@@ -0,0 +1,11 @@
+local lazy = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazy) then
+  vim.fn.system {
+    'git', 'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim',
+    '--branch=stable',
+    lazy
+  }
+end
+vim.opt.rtp:prepend(lazy)
