5dd88ec2920a1c07589fb0fe1933ac26d1935ed3
felpofo <felpofo@proton.me>
2024-02-07 17:30:43 -0300

    nvim: barbar's git and icons integration

--- nvim/lua/plugins/barbar.lua
+++ nvim/lua/plugins/barbar.lua
@@ -1,6 +1,10 @@
 return { 'romgrk/barbar.nvim',
   name = 'barbar',
   event = 'VimEnter',
+  dependencies = {
+    { 'lewis6991/gitsigns.nvim', name = 'gitsigns' },
+    'nvim-tree/nvim-web-devicons',
+  },
   opts = {
     options = {
       animation = false,
