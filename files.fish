76143af561d8b32339708a100880b6260cf1a228
felpofo <felpofo@proton.me>
2024-02-10 16:26:02 -0300

    alacritty: removed keybinds file

--- files.fish
+++ files.fish
@@ -4,7 +4,6 @@ do (pwd)/.starshiprc ~/.config/starship.toml
 do (pwd)/.tmuxrc ~/.tmux.conf
 do (pwd)/.alacrittyrc ~/.config/alacritty.toml
 do (pwd)/.alacritty.theme ~/.config/alacritty.theme.toml
-do (pwd)/.alacritty.keys ~/.config/alacritty.keybinds.toml
 
 do (pwd)/.xinitrc ~/.xinitrc
 
